45a1a55027e605c438f5f49dfa0f2464e9a84455
Vardan Melkonyan <vardan.melkonyan@tumo.org>
2021-07-07 21:39:40 +0400

    Implement anti-aliasing

--- Raytracer.cs
+++ Raytracer.cs
@@ -20,46 +20,51 @@ namespace raytracer
 			// Vector3 to_light_direction = new Vector3(-1, 1, 0).Normalized();
 			Vector3 light_position = new Vector3(4, 3, 0);
 			Vector3 light_color = new Vector3(1, 1, 1) * 5;
+			Random rnd = new Random();
+			int rays = 4;
+			Vector3[] colors = new Vector3[16];
 			for (int x = 0; x < surface.width; x++)
 			{
 				for (int y = 0; y < surface.height; y++)
 				{
-					Ray r = camera.GetCameraRay((x + 0.5f) / surface.width, (y + 0.5f) / surface.height);
-					Intersection intersection = scene.FindClosestIntersection(r);
-					if (intersection != null)
+					for (int r_ray = 0; r_ray < rays; r_ray++)
 					{
-						Vector3 intersection_point = r.position + r.direction * intersection.t;
-						Vector3 color = Vector3.Zero;
-						Vector3 normal = Vector3.UnitY;
-						Vector3 to_light = (light_position - intersection_point).Normalized();
-						bool hasShadow = false;
-						float intensity = 1 / ((light_position - intersection_point).Length * (light_position - intersection_point).Length);
-						Ray light_ray = new Ray(intersection_point, to_light);
-						Intersection shadow_intersection = scene.FindClosestIntersection(light_ray);
-						if (shadow_intersection != null && shadow_intersection.t > 0)
+						Ray r = camera.GetCameraRay((x + (float)rnd.NextDouble()) / surface.width, (y + (float)rnd.NextDouble()) / surface.height);
+						Intersection intersection = scene.FindClosestIntersection(r);
+						if (intersection != null)
 						{
-							hasShadow = true;
-						}
-						if (intersection.intersectable is Sphere)
-						{
-							Sphere sphere = intersection.intersectable as Sphere;
-							normal = (intersection_point - sphere.center).Normalized();
-							color = sphere.color;
-						}
-						else if (intersection.intersectable is Plane)
-						{
-							normal = Vector3.UnitY;
-							color = (intersection.intersectable as Plane).color;
+							Vector3 intersection_point = r.position + r.direction * intersection.t;
+							Vector3 color = Vector3.Zero;
+							Vector3 normal = Vector3.UnitY;
+							Vector3 to_light = (light_position - intersection_point).Normalized();
+							bool hasShadow = false;
+							float intensity = 1 / ((light_position - intersection_point).Length * (light_position - intersection_point).Length);
+							Ray light_ray = new Ray(intersection_point, to_light);
+							Intersection shadow_intersection = scene.FindClosestIntersection(light_ray);
+							if (shadow_intersection != null && shadow_intersection.t > 0)
+							{
+								hasShadow = true;
+							}
+							if (intersection.intersectable is Sphere)
+							{
+								Sphere sphere = intersection.intersectable as Sphere;
+								normal = (intersection_point - sphere.center).Normalized();
+								color = sphere.color;
+							}
+							else if (intersection.intersectable is Plane)
+							{
+								normal = Vector3.UnitY;
+								color = (intersection.intersectable as Plane).color;
 
+							}
+							float lambert = System.Math.Clamp(Vector3.Dot(to_light, normal), 0f, 1f);
+							colors[r_ray] = color * lambert * light_color * intensity * (hasShadow ? 0 : 1);
 						}
-						float lambert = System.Math.Clamp(Vector3.Dot(to_light, normal), 0f, 1f);
-						if (hasShadow)
-							surface.SetPixel(x, y, 0, 0, 0);
-						else
-						{
-							Vector3 c = color * lambert * light_color * intensity;
-							surface.SetPixel(x, y, c.X, c.Y, c.Z);
-						}
+						Vector3 c = new Vector3();
+						for (int i = 0; i < rays; i++)
+							c += colors[i];
+						c /= rays;
+						surface.SetPixel(x, y, c.X, c.Y, c.Z);
 					}
 				}
 			}
